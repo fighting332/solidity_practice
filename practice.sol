0ca9c0df46356e3f11d7d28a00608596415830b2
Xianyao Chen <73146888+fighting332@users.noreply.github.com>
2022-01-17 14:23:37 +0900

    Update practice.sol

--- practice.sol
+++ practice.sol
@@ -4,7 +4,7 @@ contract SolidityTest {
    constructor() public{
    }
    function getResult() public view returns(uint){
-      uint a = 1;
+      uint a = 3;
       uint b = 2;
       uint result = a + b;
       return result;
